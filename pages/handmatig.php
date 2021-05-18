ed1eb84345640245d01e90edf835fca6cc53ed2d
directict-tim <tim@directict.nl>
2021-05-18 14:01:01 +0200

    handmatig invoeren aanmelding

--- pages/handmatig.php
+++ pages/handmatig.php
@@ -1,87 +1,89 @@
 <?php
 // Include config file
 require_once "config/crud.php";
- 
+
 // Define variables and initialize with empty values
 $name = $address = $salary = "";
 $name_err = $address_err = $salary_err = "";
- 
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate name
     $input_name = trim($_POST["name"]);
-    if(empty($input_name)){
+    if (empty($input_name)) {
         $name_err = "Please enter a name.";
-    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+    } elseif (!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z\s]+$/")))) {
         $name_err = "Please enter a valid name.";
-    } else{
+    } else {
         $name = $input_name;
     }
-    
+
     // Validate address
     $input_address = trim($_POST["address"]);
-    if(empty($input_address)){
-        $address_err = "Please enter an address.";     
-    } else{
+    if (empty($input_address)) {
+        $address_err = "Please enter an address.";
+    } else {
         $address = $input_address;
     }
-    
+
     // Validate salary
     $input_salary = trim($_POST["salary"]);
-    if(empty($input_salary)){
-        $salary_err = "Please enter the salary amount.";     
-    } elseif(!ctype_digit($input_salary)){
+    if (empty($input_salary)) {
+        $salary_err = "Please enter the salary amount.";
+    } elseif (!ctype_digit($input_salary)) {
         $salary_err = "Please enter a positive integer value.";
-    } else{
+    } else {
         $salary = $input_salary;
     }
-    
+
     // Check input errors before inserting in database
-    if(empty($name_err) && empty($address_err) && empty($salary_err)){
+    if (empty($name_err) && empty($address_err) && empty($salary_err)) {
         // Prepare an insert statement
         $sql = "INSERT INTO employees (name, address, salary) VALUES (?, ?, ?)";
-         
-        if($stmt = mysqli_prepare($link, $sql)){
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_address, $param_salary);
-            
+
             // Set parameters
             $param_name = $name;
             $param_address = $address;
             $param_salary = $salary;
-            
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 // Records created successfully. Redirect to landing page
                 header("location: index.php");
                 exit();
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
-         
+
         // Close statement
         mysqli_stmt_close($stmt);
     }
-    
+
     // Close connection
     mysqli_close($link);
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Create Record</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        .wrapper{
+        .wrapper {
             width: 600px;
             margin: 0 auto;
         }
     </style>
 </head>
+
 <body>
     <div class="wrapper">
         <div class="container-fluid">
@@ -89,28 +91,48 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <div class="col-md-12">
                     <h2 class="mt-5 text-primary">Aanmelden - Handmatig</h2>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
                         <div class="form-group">
-                        
-                            <label>Speler</label>
-                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
-                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                            <label>Naam</label>
+                            <select name="speler">
+                                <?php
+                                $sql = mysqli_query($link, "SELECT callsign FROM speler");
+                                while ($row = $sql->fetch_assoc()) {
+                                    echo "<option value=\"{$row['callsign']}\">{$row['callsign']}</option>";
+                                }
+                                ?>
+                            </select>
                         </div>
                         <div class="form-group">
+
                             <label>School</label>
-                            <input type="text" name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
-                            <span class="invalid-feedback"><?php echo $address_err;?></span>
+                            <select name="store">
+                                <?php
+                                $sql = mysqli_query($link, "SELECT name FROM school");
+                                while ($row = $sql->fetch_assoc()) {
+                                    echo "<option value=\"{$row['name']}\">{$row['name']}</option>";
+                                }
+                                ?>
+                            </select>
                         </div>
                         <div class="form-group">
-                            <label>Toernooinaam</label>
-                            <input type="text" name="salary" class="form-control <?php echo (!empty($salary_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $salary; ?>">
-                            <span class="invalid-feedback"><?php echo $salary_err;?></span>
+                            <label>Toernooi</label>
+                            <select name="Toernooi">
+                                <?php
+                                $sql = mysqli_query($link, "SELECT description FROM toernooi");
+                                while ($row = $sql->fetch_assoc()) {
+                                    echo "<option value=\"{$row['description']}\">{$row['description']}</option>";
+                                }
+                                ?>
+                            </select>
                         </div>
-                        <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+
+                        <input type="submit" class="btn btn-primary" value="Aanmelden">
                     </form>
                 </div>
-            </div>        
+            </div>
         </div>
     </div>
 </body>
+
 </html>
