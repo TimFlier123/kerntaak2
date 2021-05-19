6709417d5155c795129bb5d69c5210d95f6737b8
directict-tim <tim@directict.nl>
2021-05-19 09:43:27 +0200

    editPlayer en editSchool in functions

--- crud/editschool.php
+++ crud/editschool.php
@@ -1,102 +1,45 @@
 <?php
-// Include config file
+include_once('functions.php');
 require_once "../config/crud.php";
  
-// Define variables and initialize with empty values
+
 $name  = "";
 $name_err = "";
  
-// Processing form data when form is submitted
-if(isset($_POST["id"]) && !empty($_POST["id"])){
-    // Get hidden input value
-    $id = $_POST["id"];
-   
-    // Validate name
-    $input_name = trim($_POST["name"]);
-    if(empty($input_name)){
-        $name_err = "Please enter a name.";
-    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $name_err = "Please enter a valid name.";
-    } else{
-        $name = $input_name;
-    }
-    
-    // Check input errors before inserting in database
-    if(empty($name_err) && empty($insertion_err) && empty($lastname_err) && empty($school_err)){
-        // Prepare an update statement
-        $sql = "UPDATE school SET name=? WHERE id=?";
-         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_name, $param_id,);
-            
-             // Set parameters
-             
-             $param_name = $name;
-             $param_id = $id;
-    
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Records updated successfully. Redirect to landing page
-                header("location: ../?page=schools");
 
-                exit();
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
-         
-        // Close statement
-        mysqli_stmt_close($stmt);
-    }
-    
-    // Close connection
-    mysqli_close($link);
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+
+   editSchool();
+
 } else{
-    // Check existence of id parameter before processing further
+
     if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
-        // Get URL parameter
-        $id =  trim($_GET["id"]);
         
-        // Prepare a select statement
+        $id =  trim($_GET["id"]);
         $sql = "SELECT * FROM school WHERE id = ?";
+
         if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "i", $param_id);
-            
-            // Set parameters
             $param_id = $id;
-            
-            // Attempt to execute the prepared statement
+
             if(mysqli_stmt_execute($stmt)){
                 $result = mysqli_stmt_get_result($stmt);
     
                 if(mysqli_num_rows($result) == 1){
-                    /* Fetch result row as an associative array. Since the result set
-                    contains only one row, we don't need to use while loop */
                     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-                    
-                    // Retrieve individual field value
                     $name = $row["name"];
                 } else{
-                    // URL doesn't contain valid id. Redirect to error page
                     header("location: error.php");
                     exit();
                 }
-                
+        
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
-        
-        // Close statement
         mysqli_stmt_close($stmt);
-        
-        // Close connection
         mysqli_close($link);
     }  else{
-        // URL doesn't contain id parameter. Redirect to error page
         header("location: error.php");
         exit();
     }
--- crud/functions.php
+++ crud/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 function deletePlayer(){
 
     require_once "../config/crud.php";
@@ -66,11 +65,109 @@ function deleteTournament(){
             echo "Oops! Something went wrong. Please try again later.";
         }
     }
-    
+
     mysqli_stmt_close($stmt);
     mysqli_close($link);
 }
 
+function editPlayer(){
+
+    global $link;
+    require_once "../config/crud.php";
+      $id = $_POST["id"];
+   
+
+      $input_name = trim($_POST["name"]);
+      if(empty($input_name)){
+          $name_err = "Please enter a name.";
+      } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+          $name_err = "Please enter a valid name.";
+      } else{
+          $name = $input_name;
+      }
+      
+      $input_insertion = trim($_POST["insertion"]);
+      if(empty($input_insertion)){
+          $insertion_err = "insetion.";     
+      } else{
+          $insertion = $input_insertion;
+      }
+      
+      $input_lastname = trim($_POST["lastname"]);
+      if(empty($input_insertion)){
+          $lastname_err = "lastname.";     
+      } else{
+          $lastname = $input_lastname;
+      }
+  
+     $input_school = trim($_POST["school"]);
+     if(empty($input_school)){
+         $school_err = "school.";     
+     } else{
+         $school = $input_school;
+     }
+      
+      if(empty($name_err) && empty($insertion_err) && empty($lastname_err) && empty($school_err)){
+          $sql = "UPDATE speler SET callsign=?, insertion=?, lastname=?, schoolID=? WHERE id=?";
+           
+          if($stmt = mysqli_prepare($link, $sql)){
+
+              mysqli_stmt_bind_param($stmt, "sssss", $param_name, $param_insertion, $param_lastname, $param_school, $param_id,);
+               $param_name = $name;
+               $param_insertion = $insertion;
+               $param_lastname = $lastname;
+               $param_school = $school;
+               $param_id = $id;
+              
+              if(mysqli_stmt_execute($stmt)){
+                  header("location: ../?page=spelers");
+                  exit();
+              } else{
+                  echo "Oops! Something went wrong. Please try again later.";
+              }
+          }
+          mysqli_stmt_close($stmt);
+      }
+      mysqli_close($link);
+}
+
+function editSchool(){
+     global $link;
+     $id = $_POST["id"];
+   
+     $input_name = trim($_POST["name"]);
+     if(empty($input_name)){
+         $name_err = "Please enter a name.";
+     } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+         $name_err = "Please enter a valid name.";
+     } else{
+         $name = $input_name;
+     }
+     
+     if(empty($name_err) && empty($insertion_err) && empty($lastname_err) && empty($school_err)){
+         $sql = "UPDATE school SET name=? WHERE id=?";
+          
+         if($stmt = mysqli_prepare($link, $sql)){
+             mysqli_stmt_bind_param($stmt, "ss", $param_name, $param_id,);
+              $param_name = $name;
+              $param_id = $id;
+     
+             if(mysqli_stmt_execute($stmt)){
+                 header("location: ../?page=schools");
+                 exit();
+             } else{
+                 echo "Oops! Something went wrong. Please try again later.";
+             }
+         }
+         mysqli_stmt_close($stmt);
+     }
+
+     mysqli_close($link);
+}
+
+function selectFromPlayers(){
+    
+}
 function giveError(){
     if (empty(trim($_GET["id"]))) {
         header("location: error.php");
