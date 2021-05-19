1d925dc5ea10635b12e3588fe017cc9ee0c8c36e
directict-tim <tim@directict.nl>
2021-05-19 16:07:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,33 @@
 <link rel="stylesheet" href="styling/style.css" type="text/css">
 
 <?php
-    include("includes/header.php");
+include("includes/header.php");
 
-    switch (@$_GET['page']) {
-        case "":
-            include("views/logged_in.php");
-            break;
-        case "spelers":
-            include("pages/spelers.php");
-            break;
-        case "schools":
-            include("pages/schools.php");
-            break;
-        case "toernooien":
-            include("pages/toernooien.php");
-            break;
-        case "handmatig":
-            include("pages/handmatig.php");
-            break;
-        case "importeren":
-            include("pages/import.php");
-            break;
-        case "toernooioverzicht":
-            include("pages/toernooioverzicht.php");
-           break;
-    }
+switch (@$_GET['page']) {
+    case "":
+        include("views/logged_in.php");
+        break;
+    case "spelers":
+        include("pages/spelers.php");
+        break;
+    case "schools":
+        include("pages/schools.php");
+        break;
+    case "toernooien":
+        include("pages/toernooien.php");
+        break;
+    case "handmatig":
+        include("pages/handmatig.php");
+        break;
+    case "importeren":
+        include("pages/import.php");
+        break;
+    case "toernooioverzicht":
+        include("pages/toernooioverzicht.php");
+        break;
+    case "uitslagenbeheren":
+        include("pages/uitslagenbeheren.php");
+        break;
+}
 
-    include("includes/footer.php");
+include("includes/footer.php");
