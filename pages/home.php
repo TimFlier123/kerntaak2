94ef946d60a868159817f358d7817dd8c0d0f3a6
directict-tim <tim@directict.nl>
2021-05-20 10:38:00 +0200

    Create home.php

---
+++ pages/home.php
@@ -0,0 +1,18 @@
+<style>
+    .center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+  }
+  .container{
+      margin-left: 25%;
+  }
+</style>
+     
+     <main role="main" class="container">
+      <h1 class="mt-5">KNLTB</h1>
+      <p class="lead">Welkom bij het wedstrijdregistratiesysteem voor het tennistoernooi MBO Open.</p>
+      <p>Gebruik de menubalk om het systeem te gebruiken.</p>
+    </main>
+    <img src="media/logo.jpg" class="center" alt="Girl in a jacket" width="500" height="600">
